846ab3b4b62a2f017d715e8f8742001ff68aa8ae
nickkrasborges <nickkrasborges@gmail.com>
2019-11-12 10:02:03 -0300

    criação do projeto.

---
+++ src/main/java/br/com/springboot/demo/autenticator/AutenticatorApplication.java
@@ -0,0 +1,13 @@
+package br.com.springboot.demo.autenticator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutenticatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutenticatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/springboot/demo/autenticator/AutenticatorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.springboot.demo.autenticator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutenticatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
